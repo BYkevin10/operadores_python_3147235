aee942997d06e4b95b9fccda59890604c903aa22
Steven <stev508ga@gmail.com>
2025-03-07 09:31:30 -0500

    Jerarquia Operadores Booleanos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -35,4 +35,14 @@ Jerarquia definitiva de operadores
 7.    AND
 8.    OR
 9.    = 
+
+NOTA: Si hay operaciones en el mismo nivel de jerarquia, se resuelven de izquierda a derecha
 '''
+
+op1= False
+op2 = True 
+op3 = False
+op4 = True
+
+resultado = not op1 and (op2 or op3 and not op1) and not op4
+print(resultado)
